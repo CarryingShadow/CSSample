fb5aee4f6c16847a72e9a78d2b66edaa36d9e9d0
谭晓军 <tanxiaojun@10086.im>
2016-03-29 11:53:41 +0800

    Initial commit

---
+++ app/src/main/java/com/shadow/cssample/ShadowActivity.java
@@ -0,0 +1,13 @@
+package com.shadow.cssample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ShadowActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shadow);
+    }
+}
